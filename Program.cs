9768c7b7e7f1750e251fd65b0192540bd05bc36a
Marquis Pelletier <marquispelletier@gmail.com>
2025-12-19 19:46:19 -0500

    added that when someone is typing we see the user

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
-﻿using NetCord;
+﻿using System.Runtime.CompilerServices;
+using NetCord;
 using NetCord.Gateway;
 using NetCord.Logging;
 
 string discordToken = Environment.GetEnvironmentVariable("DISCORD_TOKEN") ?? throw new InvalidOperationException("DISCORD_TOKEN environment variable is not set.");
 GatewayClient client = new(new BotToken(discordToken), new GatewayClientConfiguration
 {
-    Intents = GatewayIntents.GuildMessages | GatewayIntents.MessageContent | GatewayIntents.GuildPresences,
+        Intents = GatewayIntents.GuildMessages 
+            | GatewayIntents.MessageContent 
+            | GatewayIntents.GuildPresences
+            | GatewayIntents.GuildMessageTyping
+            | GatewayIntents.DirectMessageTyping, 
 });
 
 client.MessageCreate += async message =>
@@ -26,6 +31,17 @@ client.PresenceUpdate += async presence =>
     Console.WriteLine($"User {presence.User.Username} is now {presence.Status}");
     return;
 };
+
+client.TypingStart += async typing =>
+{
+    var user = typing.User;
+    if(user != null)
+    {   
+        Console.WriteLine($"User {user.Username} is now typing");
+    }
+};
+
+
 
 await client.StartAsync();
 await Task.Delay(-1);
